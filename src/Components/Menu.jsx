eea8281ce3b23d8dd1627db7aaf47a0922bf7495
surya <cpgnsc@gmail.com>
2022-11-14 22:02:07 +0600

    __||#57||__

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import '../index.css'
 const Menu = () => {
   return (
     <div>
-    <Link to='/about'>AboutUs</Link>
-    <Link to='/contact'>Contact</Link>
+    <NavLink exact activeClassName='active_class' to='/about'>AboutUs</NavLink>
+   
+     <NavLink activeClassName='active_class' to='/contact'>Contact</NavLink>
 
 
     </div>
